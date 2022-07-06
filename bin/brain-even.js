ad96ae81ebe97246ebb71e57daf33a6a44403e19
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-07 01:29:18 +0300

    Refactor win game logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -11,11 +11,8 @@ import {
   printCongratsMessage,
 } from '../src/index.js';
 
-const AVAILABLE_ANSWERS = ['yes', 'no'];
-const GAME_CYCLES_TOTAL_NUMBER = 3;
-
 const evenNumberGame = () => {
-  let correctAnswersCounter = 0;
+  const GAME_CYCLES_TOTAL_NUMBER = 3;
 
   const userName = printGreeting();
   printGameDescription(
@@ -24,29 +21,21 @@ const evenNumberGame = () => {
 
   for (let i = 1; i <= GAME_CYCLES_TOTAL_NUMBER; i++) {
     const targetNumber = getRandomNumber(100);
-    printQuestion(targetNumber);
-    const correctAnswer = isEven(targetNumber) ? 'yes' : 'no';
-    const userAnswer = readLineSync.question('Your answer: ');
 
-    if (!AVAILABLE_ANSWERS.includes(userAnswer)) {
-      printTryAgainMessage();
-      return;
-    }
+    printQuestion(targetNumber);
+
+    const userAnswer = readLineSync.question('Your answer: ');
+    const correctAnswer = isEven(targetNumber) ? 'yes' : 'no';
 
     if (userAnswer !== correctAnswer) {
       printTryAgainMessage(userName, userAnswer, correctAnswer);
       return;
     }
 
-    if (userAnswer === correctAnswer) {
-      printCorrectMessage();
-      correctAnswersCounter += 1;
-    }
+    printCorrectMessage;
   }
 
-  if (correctAnswersCounter === GAME_CYCLES_TOTAL_NUMBER) {
-    printCongratsMessage(userName);
-  }
+  printCongratsMessage(userName);
 };
 
 evenNumberGame();
