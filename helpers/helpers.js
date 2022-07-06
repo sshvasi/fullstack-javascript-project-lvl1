22393e8c858c47c981dc6855aa0793136c8abcc4
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-07 01:23:12 +0300

    Fix random number fn

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,9 @@
 const isEven = (number) => number % 2 === 0;
 
-const getRandomNumber = (max) => Math.floor(Math.random() * max);
+const getRandomNumber = (min, max) => {
+  const randomFloat = min + Math.random() * (max + 1 - min);
+  const randomInteger = Math.floor(randomFloat);
+  return randomInteger;
+};
 
 export { isEven, getRandomNumber };
