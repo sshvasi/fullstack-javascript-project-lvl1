8ad8624c713cde552404e60967285d1f7028380c
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-07 02:45:12 +0300

    Add calc game logic

---
+++ bin/brain-calc.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import { getRandomNumber } from '../src/helpers.js';
+import startGameLoop from '../src/index.js';
+
+const calcGame = () => {
+  const GAME_DESCRIPTION = 'What is the result of the expression?';
+
+  const operations = ['-', '+', '*'];
+
+  const methods = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+  };
+
+  const evaluate = (expression) => {
+    const split = expression.split(' ');
+    const a = +split[0];
+    const op = split[1];
+    const b = +split[2];
+
+    if (!methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return methods[op](a, b);
+  };
+
+  const generateGameEntry = () => {
+    const a = getRandomNumber(0, 10);
+    const b = getRandomNumber(0, 10);
+    const op = operations[getRandomNumber(0, 2)];
+
+    const targetExpression = `${a} ${op} ${b}`;
+    const correctAnswer = String(evaluate(targetExpression));
+
+    return [targetExpression, correctAnswer];
+  };
+
+  startGameLoop(GAME_DESCRIPTION, generateGameEntry);
+};
+
+calcGame();
