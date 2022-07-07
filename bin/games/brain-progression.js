44bbaa1515aec4e79337591495932cc4880fb661
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-07 14:32:59 +0300

    Add progression game

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import startGameLoop from '../../src/index.js';
+import { getRandomNumber } from '../../src/helpers.js';
+
+const progressionGame = () => {
+  const GAME_DESCRIPTION = 'What number is missing in the progression?';
+
+  const getProgression = (start, step, length) => {
+    let progression = [];
+
+    for (let i = 0; i < length; i++) {
+      progression = [...progression, start + step * i];
+    }
+
+    return progression;
+  };
+
+  const generateGameEntry = () => {
+    const length = 10;
+    const start = getRandomNumber(0, 50);
+    const step = getRandomNumber(1, 5);
+
+    const progression = getProgression(start, step, length);
+    const missingElIndex = getRandomNumber(0, -1);
+
+    const correctAnswer = String(progression.splice(missingElIndex, 1, '..'));
+    const progressionWithMissingEl = progression.join(' ');
+
+    return [progressionWithMissingEl, correctAnswer];
+  };
+
+  startGameLoop(GAME_DESCRIPTION, generateGameEntry);
+};
+
+progressionGame();
